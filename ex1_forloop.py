12df03c03018e1a322a54346328b5f4026a844a3
moulimallina <moulimlsn172@gmail.com>
2024-01-31 07:21:03 -0600

    Modified a file ex1 from devteam1 first time

--- ex1_forloop.py
+++ ex1_forloop.py
@@ -6,3 +6,5 @@ print("Message to check .gitignore file")
 print("Added this line of code from techdevelopers from git terminal")
 #-----------------------------------------------------
 print("Added this line of code from Github for first time")
+#-----------------------------------------------------
+print("Added this line of code from devteam1 for first time and commit")
