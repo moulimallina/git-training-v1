7a8c43b5dace262a962a974ddaf715dcfdb1af9f
techdevlopers <moulimallina122@gmail.com>
2024-01-31 06:26:57 -0600

    commit from new author techdevlopers

--- ex1_forloop.py
+++ ex1_forloop.py
@@ -2,3 +2,5 @@ print("first modification for secnd commit")
 print("Added to see checkout")
 #-----------------------------------------------------
 print("Message to check .gitignore file")
+#-----------------------------------------------------
+print("Added this line of code from techdevelopers from git terminal")
